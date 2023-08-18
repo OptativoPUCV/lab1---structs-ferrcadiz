14d203aab1ea96986ef3cf063aa600f0ed681824
Replit user <>
2023-08-18 02:37:34 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -187,4 +187,14 @@ typedef struct nodo {
   struct nodo *siguiente; // puntero al siguiente nodo
 } Nodo;
 
-Nodo *crearListaEnlazada(int arr[], int size) { return NULL; }
+Nodo *crearListaEnlazada(int arr[], int size) { 
+  Nodo *primerNodo = NULL;
+  Nodo *ultimoNodo = NULL;
+  
+  for(int i = 0 ; i < size ; i++){
+    Nodo *nuevoNodo = (Nodo *) malloc(sizeof(Nodo));
+  if(nuevoNodo == NULL) return NULL;
+  }
+  
+  return NULL; 
+}
